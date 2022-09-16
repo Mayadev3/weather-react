373025b9ef91e239bd742c0b6bca8faa7aa6b6fe
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-09-16 13:05:00 +0200

    added axios and react loader spinner

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Weather.css";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${props.city} is ${Math.round(response.data.main.temp)}`
+    );
+  }
+  let apiKey = "8ffe8ebc319a3f920065447a31ce0df0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return;
+}
