85e89bce0d2dcb4ded9721b11f9cc00e2f4170cc
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-09-27 01:04:56 +0200

    added alternative text to my image

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -9,13 +9,13 @@ export default function WeatherForescast(props) {
   let longitude = props.coordinates.lon;
   let latitude = props.coordinates.lat;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
+  //axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <p className=" day ms-2 mb-5">Thu</p>
-          <img src="/images/sun.png" className="forecastImg ms-1" />
+          <img src="/images/sun.png" alt="sun" className="forecastImg ms-1" />
           <div className="tempExtremes">
             <span className="maxTemp me-2">35°</span>
             <span className="minTemp">20°</span>
